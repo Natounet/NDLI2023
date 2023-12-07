7ae064aba54bdef8234740e948a80222c00df2cc
Natounet <naytan@protonmail.com>
2023-12-07 19:05:02 +0100

    carteSuivante

--- logic.js
+++ logic.js
@@ -5,13 +5,25 @@ var agriculture = 0;
 var chauffage = 0;
 var carteJoues = [];
 var allCartes = [];
+function carteSuivante() {
+    // On cherche a chercher une carte qui n'a pas été joué et dont les prérequis sont satisfait
+    var carte;
+    while (true) {
+        var random = Math.floor(Math.random() * allCartes.length);
+        carte = allCartes[random];
+        if (!(carte.id in carteJoues) && carte.estJouable()) {
+            return carte;
+        }
+    }
+}
 var Carte = /** @class */ (function () {
-    function Carte(id, nom, description, effets, prerequis) {
+    function Carte(id, nom, description, effets, prerequis, source) {
         this.id = id;
         this.nom = nom;
         this.description = description;
         this.effets = effets;
         this.prerequis = prerequis;
+        this.source = source;
     }
     Carte.prototype.jouerCarte = function () {
         // Vérification si les prérequis sont satisfait.
--- logic.ts
+++ logic.ts
@@ -7,6 +7,22 @@ let chauffage = 0;
 let carteJoues: number[] = [];
 let allCartes: Carte[] = [];
 
+function carteSuivante(): Carte {
+    // On cherche a chercher une carte qui n'a pas été joué et dont les prérequis sont satisfait
+    let carte: Carte;
+
+    while(true){
+
+        let random:number = Math.floor(Math.random() * allCartes.length);
+
+        carte = allCartes[random];
+        if(!(carte.id in carteJoues) && carte.estJouable()){
+            return carte;
+        }
+
+    }
+}
+
 type effet = {
     industrie: number;
     transport: number;
@@ -20,13 +36,15 @@ class Carte {
     description: string;
     effets: effet;
     prerequis: number[];
+    source: string;
 
-    constructor(id: number, nom: string, description: string, effets: effet, prerequis: number[]) {
+    constructor(id: number, nom: string, description: string, effets: effet, prerequis: number[], source: string) {
         this.id = id;
         this.nom = nom;
         this.description = description;
         this.effets = effets;
         this.prerequis = prerequis;
+        this.source = source;
     }
 
     jouerCarte(){
