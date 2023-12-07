ad78ed18944e0da955de763d63fed679f12f75dc
Valentin Regnault <56791498+ValentinRegnault@users.noreply.github.com>
2023-12-07 23:47:43 +0100

    formattage de logic.ts

--- public/logic.ts
+++ public/logic.ts
@@ -14,7 +14,7 @@ type effet = {
     transport: number;
     agriculture: number;
     logement: number;
-}
+};
 
 class Carte {
     id: number;
@@ -25,7 +25,15 @@ class Carte {
     source: string;
     info: string;
 
-    constructor(id: number, nom: string, description: string, effets: effet, prerequis: number[], source: string, info: string) {
+    constructor(
+        id: number,
+        nom: string,
+        description: string,
+        effets: effet,
+        prerequis: number[],
+        source: string,
+        info: string
+    ) {
         this.id = id;
         this.nom = nom;
         this.description = description;
@@ -35,11 +43,7 @@ class Carte {
         this.info = info;
     }
 
-
-
-
     boutonVert() {
-
         if (boutonVertRestant > 0) {
             // Vérification si les prérequis sont satisfait.
             if (this.estJouable()) {
@@ -53,14 +57,9 @@ class Carte {
 
             boutonVertRestant -= 1;
         }
-
-
-
-
     }
 
     boutonRouge() {
-
         // Vérification si les prérequis sont satisfait.
         if (this.estJouable()) {
             agriculture -= this.effets.agriculture;
@@ -70,28 +69,20 @@ class Carte {
 
             carteJoues.push(this.id);
         }
-
-
     }
 
     estJouable(): boolean {
-
-        this.prerequis.forEach(pr => {
-
+        this.prerequis.forEach((pr) => {
             // Prérequis non satisfait
             if (!(pr in carteJoues)) {
                 return false;
             }
-
-
         });
 
         return true;
     }
 }
 let allCartes: Carte[] = [];
-
-
 
 async function creerCarte(): Promise<Carte[]> {
     let allCartes: Carte[] = [];
@@ -101,43 +92,44 @@ async function creerCarte(): Promise<Carte[]> {
     let data = await response.json();
     let cartes = Object.values(data);
 
-    cartes.forEach((obj: any) => allCartes.push(new Carte(obj.id, obj.nom, obj.description, obj.effets, obj.prerequis, obj.source, obj.info)));
+    cartes.forEach((obj: any) =>
+        allCartes.push(
+            new Carte(
+                obj.id,
+                obj.nom,
+                obj.description,
+                obj.effets,
+                obj.prerequis,
+                obj.source,
+                obj.info
+            )
+        )
+    );
 
     return allCartes;
-
 }
-
-
 
 function partieTermine(): boolean {
-    if (industrie < 100 && transport < 100 && agriculture < 100 && logement < 100) {
-        
-            if (carteJoues.length != allCartes.length) {
-                console.log("Partie non terminé")
-                return false;
-            }
-        }
-        
-    console.log("Partie terminé")
-    return true;
+    return (
+        (industrie > 100 ||
+            transport > 100 ||
+            agriculture > 100 ||
+            logement > 100) &&
+        carteJoues.length <= allCartes.length
+    );
 }
-
-
-
 
 function carteSuivante(): Carte {
     // On cherche a chercher une carte qui n'a pas été joué et dont les prérequis sont satisfait
     let carte: Carte;
 
     while (true) {
-
         let random: number = Math.floor(Math.random() * allCartes.length);
 
         carte = allCartes[random];
         if (!(carte.id in carteJoues) && carte.estJouable()) {
             return carte;
         }
-
     }
 }
 
@@ -145,89 +137,84 @@ function genererAffichageCarte(carte: Carte): string {
     return `<div class="carte"><h3>${carte.nom}</h3><div class="carte_interieur"><p>${carte.description}</p></div></div>`;
 }
 
-
 async function main() {
+    allCartes = await creerCarte();
+    console.log(allCartes);
 
-    allCartes = await creerCarte()
-    console.log(allCartes)
-
-    function boucle(){
+    function boucle() {
         let carteActuelle = carteSuivante();
-        let cartehtml = genererAffichageCarte(carteActuelle)
-        let div = document.createElement('div');
-    
-        div.innerHTML = cartehtml
-        document.body.appendChild(div)
-    
-    
-    
-        
-    
-        div.addEventListener('mousedown', (event) => {
+        let cartehtml = genererAffichageCarte(carteActuelle);
+        let div = document.createElement("div");
+
+        div.innerHTML = cartehtml;
+        document.body.appendChild(div);
+
+        div.addEventListener("mousedown", (event) => {
             // Listen for mousemove events on the document
-            document.addEventListener('mousemove', rotateCard);
+            document.addEventListener("mousemove", rotateCard);
         });
-    
+
         // Listen for mouseup events on the document
         let startX: number;
-    
-        document.addEventListener('mousedown', (event: MouseEvent) => {
+
+        document.addEventListener("mousedown", (event: MouseEvent) => {
             startX = event.clientX;
             // Listen for mousemove events on the document
-            document.addEventListener('mousemove', rotateCard);
+            document.addEventListener("mousemove", rotateCard);
         });
-    
-        document.addEventListener('mouseup', (event: MouseEvent) => {
+
+        document.addEventListener("mouseup", (event: MouseEvent) => {
             // Remove the mousemove event listener
-            document.removeEventListener('mousemove', rotateCard);
+            document.removeEventListener("mousemove", rotateCard);
             // Check if the card is dragged to the left or right
             let endX = event.clientX;
-            let direction = endX > startX ? 'right' : 'left';
-    
-            if (direction == 'right') {
-                console.log(carteActuelle)
+            let direction = endX > startX ? "right" : "left";
+
+            if (direction == "right") {
+                console.log(carteActuelle);
                 carteActuelle.boutonVert();
                 // add translation to go outside of the screen
                 let card = div.getElementsByTagName("div")[0];
-                card.style.transition = 'transform 1s ease-out, opacity 1s ease-out';
+                card.style.transition =
+                    "transform 1s ease-out, opacity 1s ease-out";
                 card.style.transform = `translateX(400%)`;
-                card.style.opacity = '0';
-    
-            
+                card.style.opacity = "0";
+
                 // remove the card from the DOM after the animation is finished
                 setTimeout(() => {
                     div.remove();
                 }, 1000);
-            
-            }
-
-            else {
+            } else {
                 carteActuelle.boutonRouge();
                 // add translation to go outside of the screen
                 let card = div.getElementsByTagName("div")[0];
-                card.style.transition = 'transform 1s ease-out, opacity 1s ease-out';
+                card.style.transition =
+                    "transform 1s ease-out, opacity 1s ease-out";
                 card.style.transform = `translateX(-400%)`;
-                card.style.opacity = '0';
-    
-            
+                card.style.opacity = "0";
+
                 // remove the card from the DOM after the animation is finished
                 setTimeout(() => {
                     div.remove();
                 }, 1000);
             }
 
-            if(!partieTermine()){
+            if (!partieTermine()) {
                 boucle();
             }
-
         });
-    
+
         function rotateCard(event: MouseEvent) {
             // Calculate the rotation angle based on the mouse position
-            let rotationAngle = (event.clientX - window.innerWidth / 2) / (window.innerWidth / 2) * 30; // Adjust the divisor to change the rotation speed
-    
+            let rotationAngle =
+                ((event.clientX - window.innerWidth / 2) /
+                    (window.innerWidth / 2)) *
+                30; // Adjust the divisor to change the rotation speed
+
             // Rotate the card
-            div.getElementsByTagName("div")[0].style.transform = `translateX(-50%) rotate(${rotationAngle}deg)`;
+            div.getElementsByTagName(
+                "div"
+            )[0].style.transform = `translateX(-50%) rotate(${rotationAngle}deg)`;
         }
     }
 
@@ -237,18 +224,6 @@ async function main() {
     }
 
     boucle();
-
-
-   
-
-
-
-
 }
 
 main();
-
-
-
-
-
