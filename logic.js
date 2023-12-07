56468eb0032bb9d28b26b1a31acee471d1ea1a91
Natounet <naytan@protonmail.com>
2023-12-07 20:45:51 +0100

    Code marche lol

--- logic.js
+++ logic.js
@@ -5,30 +5,51 @@ var temperature = 0;
 var industrie = 0;
 var transport = 0;
 var agriculture = 0;
-var chauffage = 0;
+var logement = 0;
 var carteJoues = [];
-var allCartes = [];
-// Main logic
-creerCarte();
-function creerCarte() {
-    fs.readFile('cartes.json', 'utf-8', function (err, data) {
-        if (err) {
-            console.error('Error reading file:', err);
-            return;
+var Carte = /** @class */ (function () {
+    function Carte(id, nom, description, effets, prerequis, source, info) {
+        this.id = id;
+        this.nom = nom;
+        this.description = description;
+        this.effets = effets;
+        this.prerequis = prerequis;
+        this.source = source;
+        this.info = info;
+    }
+    Carte.prototype.jouerCarte = function () {
+        // Vérification si les prérequis sont satisfait.
+        if (this.estJouable()) {
+            agriculture += this.effets.agriculture;
+            industrie += this.effets.industrie;
+            transport += this.effets.transport;
+            logement += this.effets.logement;
+            carteJoues.push(this.id);
         }
-        // Parse the JSON string into an array of objects
-        var objArray = JSON.parse(data);
-        var cartes = Object.values(objArray);
-        // Map each object to a new Carte instance and push them to allCartes
-        cartes.forEach(function (obj) { return allCartes.push(new Carte(obj.id, obj.nom, obj.description, obj.effets, obj.prerequis, obj.source, obj.info)); });
-        // Now allCartes array is filled with 'Carte' objects
-        console.log(allCartes);
-    });
+    };
+    Carte.prototype.estJouable = function () {
+        this.prerequis.forEach(function (pr) {
+            // Prérequis non satisfait
+            if (!(pr in carteJoues)) {
+                return false;
+            }
+        });
+        return true;
+    };
+    return Carte;
+}());
+var allCartes = creerCarte();
+function creerCarte() {
+    var allCartes = [];
+    var data = fs.readFileSync('cartes.json', 'utf-8');
+    var objArray = JSON.parse(data);
+    var cartes = Object.values(objArray);
+    cartes.forEach(function (obj) { return allCartes.push(new Carte(obj.id, obj.nom, obj.description, obj.effets, obj.prerequis, obj.source, obj.info)); });
+    return allCartes;
 }
-console.log(allCartes);
 function partieTermine() {
     if (temperature <= 10 && temperature <= -10) {
-        if (industrie < 100 && transport < 100 && agriculture < 100 && chauffage < 100) {
+        if (industrie < 100 && transport < 100 && agriculture < 100 && logement < 100) {
             if (carteJoues.length != allCartes.length) {
                 return false;
             }
@@ -47,34 +68,6 @@ function carteSuivante() {
         }
     }
 }
-var Carte = /** @class */ (function () {
-    function Carte(id, nom, description, effets, prerequis, source, info) {
-        this.id = id;
-        this.nom = nom;
-        this.description = description;
-        this.effets = effets;
-        this.prerequis = prerequis;
-        this.source = source;
-        this.info = info;
-    }
-    Carte.prototype.jouerCarte = function () {
-        // Vérification si les prérequis sont satisfait.
-        if (this.estJouable()) {
-            agriculture += this.effets.agriculture;
-            industrie += this.effets.industrie;
-            transport += this.effets.transport;
-            chauffage += this.effets.chauffage;
-            carteJoues.push(this.id);
-        }
-    };
-    Carte.prototype.estJouable = function () {
-        this.prerequis.forEach(function (pr) {
-            // Prérequis non satisfait
-            if (!(pr in carteJoues)) {
-                return false;
-            }
-        });
-        return true;
-    };
-    return Carte;
-}());
+function main() {
+}
+main();
--- logic.ts
+++ logic.ts
@@ -3,40 +3,89 @@ let temperature = 0;
 let industrie = 0;
 let transport = 0;
 let agriculture = 0;
-let chauffage = 0;
+let logement = 0;
 
 let carteJoues: number[] = [];
-let allCartes: Carte[] = [];
 
 // Main logic
 
-creerCarte();
+type effet = {
+    industrie: number;
+    transport: number;
+    agriculture: number;
+    logement: number;
+}
 
-function creerCarte(){
-    fs.readFile('cartes.json','utf-8', (err, data) => {
-        if (err) {
-            console.error('Error reading file:', err);
-            return;
-        }
-    
-        // Parse the JSON string into an array of objects
-        let objArray = JSON.parse(data);
-        let cartes = Object.values(objArray);
-        // Map each object to a new Carte instance and push them to allCartes
-        cartes.forEach((obj:any) => allCartes.push(new Carte(obj.id, obj.nom, obj.description, obj.effets, obj.prerequis, obj.source, obj.info)));
-    
-        // Now allCartes array is filled with 'Carte' objects
-        console.log(allCartes);
+class Carte {
+    id: number;
+    nom: string;
+    description: string;
+    effets: effet;
+    prerequis: number[];
+    source: string;
+    info: string;
+
+    constructor(id: number, nom: string, description: string, effets: effet, prerequis: number[], source: string, info: string) {
+        this.id = id;
+        this.nom = nom;
+        this.description = description;
+        this.effets = effets;
+        this.prerequis = prerequis;
+        this.source = source;
+        this.info = info;
     }
 
-)}
+    jouerCarte() {
 
-console.log(allCartes)
+        // Vérification si les prérequis sont satisfait.
+        if (this.estJouable()) {
+            agriculture += this.effets.agriculture;
+            industrie += this.effets.industrie;
+            transport += this.effets.transport;
+            logement += this.effets.logement;
+
+            carteJoues.push(this.id);
+        }
+
+
+    }
+
+    estJouable(): boolean {
+
+        this.prerequis.forEach(pr => {
+
+            // Prérequis non satisfait
+            if (!(pr in carteJoues)) {
+                return false;
+            }
+
+
+        });
+
+        return true;
+    }
+}
+let allCartes: Carte[] = creerCarte();
+
+
+function creerCarte(): Carte[] {
+    let allCartes: Carte[] = [];
+
+    let data = fs.readFileSync('cartes.json', 'utf-8');
+    let objArray = JSON.parse(data);
+    let cartes = Object.values(objArray);
+
+    cartes.forEach((obj: any) => allCartes.push(new Carte(obj.id, obj.nom, obj.description, obj.effets, obj.prerequis, obj.source, obj.info)));
+
+    return allCartes;
+
+}
+
 
 
 function partieTermine(): boolean {
     if (temperature <= 10 && temperature <= -10) {
-        if (industrie < 100 && transport < 100 && agriculture < 100 && chauffage < 100) {
+        if (industrie < 100 && transport < 100 && agriculture < 100 && logement < 100) {
             if (carteJoues.length != allCartes.length) {
                 return false;
             }
@@ -64,62 +113,13 @@ function carteSuivante(): Carte {
     }
 }
 
-type effet = {
-    industrie: number;
-    transport: number;
-    agriculture: number;
-    chauffage: number;
+function main() {
+
+
 }
 
-class Carte {
-    id: number;
-    nom: string;
-    description: string;
-    effets: effet;
-    prerequis: number[];
-    source: string;
-    info: string;
+main()
 
-    constructor(id: number, nom: string, description: string, effets: effet, prerequis: number[], source: string, info:string) {
-        this.id = id;
-        this.nom = nom;
-        this.description = description;
-        this.effets = effets;
-        this.prerequis = prerequis;
-        this.source = source;
-        this.info = info;
-    }
-
-    jouerCarte() {
-
-        // Vérification si les prérequis sont satisfait.
-        if (this.estJouable()) {
-            agriculture += this.effets.agriculture;
-            industrie += this.effets.industrie;
-            transport += this.effets.transport;
-            chauffage += this.effets.chauffage;
-
-            carteJoues.push(this.id);
-        }
-
-
-    }
-
-    estJouable(): boolean {
-
-        this.prerequis.forEach(pr => {
-
-            // Prérequis non satisfait
-            if (!(pr in carteJoues)) {
-                return false;
-            }
-
-
-        });
-
-        return true;
-    }
-}
 
 
 
