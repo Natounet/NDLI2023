d9400b9eb948dc63937013e86248fd0134f1b71f
Natounet <naytan@protonmail.com>
2023-12-07 18:43:55 +0100

    Logique de base

--- logic.js
+++ logic.js
@@ -1,3 +1,9 @@
+var temperature = 0;
+var industrie = 0;
+var transport = 0;
+var agriculture = 0;
+var chauffage = 0;
+var carteJoués = [];
 var Carte = /** @class */ (function () {
     function Carte(id, nom, description, effets, prerequis) {
         this.id = id;
@@ -6,5 +12,17 @@ var Carte = /** @class */ (function () {
         this.effets = effets;
         this.prerequis = prerequis;
     }
+    Carte.prototype.jouerCarte = function () {
+        // Vérification si les prérequis sont satisfait.
+    };
+    Carte.prototype.estJouable = function () {
+        this.prerequis.forEach(function (pr) {
+            // Prérequis non satisfait
+            if (!(pr in carteJoués)) {
+                return false;
+            }
+        });
+        return true;
+    };
     return Carte;
 }());
--- logic.ts
+++ logic.ts
@@ -1,21 +1,54 @@
+let temperature = 0;
+let industrie = 0;
+let transport = 0;
+let agriculture = 0;
+let chauffage = 0;
+
+let carteJoués = [];
+
+type effet = {
+    industrie: number;
+    transport: number;
+    agriculture: number;
+    chauffage: number;
+}
+
 class Carte {
     id: number;
     nom: string;
     description: string;
-    effets: {
-        industrie: number;
-        transport: number;
-        agriculture: number;
-        chauffage: number;
-    }
+    effets: effet;
     prerequis: number[];
 
-    constructor(id: number, nom: string, description: string, effets: , prerequis) {
+    constructor(id: number, nom: string, description: string, effets: effet, prerequis: number[]) {
         this.id = id;
         this.nom = nom;
         this.description = description;
         this.effets = effets;
         this.prerequis = prerequis;
+    }
+
+    jouerCarte(){
+
+        // Vérification si les prérequis sont satisfait.
+
+        
+
+    }
+
+    estJouable(): boolean {
+
+        this.prerequis.forEach(pr => {
+
+            // Prérequis non satisfait
+            if(!(pr in carteJoués)){
+                return false;
+            }
+
+            
+        });
+
+        return true;
     }
 
 
